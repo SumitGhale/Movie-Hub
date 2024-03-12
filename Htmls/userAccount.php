e40bae5490c1566da8da700ba8dd7df6c27adbb7
SumitGhale <grgsten@gmail.com>
2024-03-12 19:49:08 +1100

    edited user account added input to the profile div

--- Htmls/userAccount.php
+++ Htmls/userAccount.php
@@ -21,52 +21,150 @@
                 <p>Johnkumar@gmail.com</p>
             </div>
         </div>
-        <button type="button" class="btn btn-primary">Primary</button>
+        <button type="button" class="btn btn-primary">Logout</button>
     </div>
 
     <!-- profile options -->
     <div id="profile_details">
-        <div class="d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary" style="width: 280px;">
+        <div class="d-flex flex-column flex-shrink-0 p-3 bg-white" style="width: 280px;">
             <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
                 <span class="fs-4">Sidebar</span>
             </a>
             <hr>
             <ul class="nav nav-pills flex-column mb-auto">
                 <li class="nav-item">
-                    <a href="#" class="nav-link active" aria-current="page">
-                        Home
+                    <a href="#" id="profile_btn" class="nav-link active side_btns" aria-current="page">
+                        Profile
                     </a>
                 </li>
                 <li>
-                    <a href="#" class="nav-link link-body-emphasis">
-                        Dashboard
+                    <a href="#" id="watchlists_btn" class="nav-link link-body-emphasis side_btns">
+                        Watchlists
                     </a>
                 </li>
                 <li>
-                    <a href="#" class="nav-link link-body-emphasis">
-                        Orders
+                    <a href="#" id="yourlists_btn" class="nav-link link-body-emphasis side_btns">
+                        Your Lists
                     </a>
                 </li>
                 <li>
-                    <a href="#" class="nav-link link-body-emphasis">
-                        Products
+                    <a href="#" id="account_settings_btn" class="nav-link link-body-emphasis side_btns">
+                        Account Settings
                     </a>
                 </li>
                 <li>
-                    <a href="#" class="nav-link link-body-emphasis">
-                        Customers
+                    <a href="#" id="logout_btn" class="nav-link link-body-emphasis side_btns">
+                        Logout
                     </a>
                 </li>
             </ul>
             <hr>
         </div>
 
-        <div id="user_details">
-            user details
+        <div class="sections">
+            <div class="section" id="profile">
+                <div class="row g-3">
+                    <div class="col-sm-6">
+                        <label for="firstName" class="form-label">First name</label>
+                        <input type="text" class="form-control" id="firstName" placeholder="" value="" required="" fdprocessedid="dmr3i">
+                        <div class="invalid-feedback">
+                            Valid first name is required.
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6">
+                        <label for="lastName" class="form-label">Last name</label>
+                        <input type="text" class="form-control" id="lastName" placeholder="" value="" required="" fdprocessedid="kxpm7">
+                        <div class="invalid-feedback">
+                            Valid last name is required.
+                        </div>
+                    </div>
+
+                    <div class="col-12">
+                        <label for="username" class="form-label">Username</label>
+                        <div class="input-group has-validation">
+                            <span class="input-group-text">@</span>
+                            <input type="text" class="form-control" id="username" placeholder="Username" required="" fdprocessedid="h2z37i">
+                            <div class="invalid-feedback">
+                                Your username is required.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-12">
+                        <label for="email" class="form-label">Email <span class="text-body-secondary">(Optional)</span></label>
+                        <input type="email" class="form-control" id="email" placeholder="you@example.com" fdprocessedid="w2fedd">
+                        <div class="invalid-feedback">
+                            Please enter a valid email address for shipping updates.
+                        </div>
+                    </div>
+
+                    <div class="col-12">
+                        <label for="address" class="form-label">Address</label>
+                        <input type="text" class="form-control" id="address" placeholder="1234 Main St" required="" fdprocessedid="fyysxg">
+                        <div class="invalid-feedback">
+                            Please enter your shipping address.
+                        </div>
+                    </div>
+
+                    <div class="col-12">
+                        <label for="address2" class="form-label">Address 2 <span class="text-body-secondary">(Optional)</span></label>
+                        <input type="text" class="form-control" id="address2" placeholder="Apartment or suite" fdprocessedid="1dkqqa">
+                    </div>
+
+                    <div class="col-md-5">
+                        <label for="country" class="form-label">Country</label>
+                        <select class="form-select" id="country" required="" fdprocessedid="doj4hm">
+                            <option value="">Choose...</option>
+                            <option>United States</option>
+                        </select>
+                        <div class="invalid-feedback">
+                            Please select a valid country.
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <label for="state" class="form-label">State</label>
+                        <select class="form-select" id="state" required="" fdprocessedid="8pjd6d">
+                            <option value="">Choose...</option>
+                            <option>California</option>
+                        </select>
+                        <div class="invalid-feedback">
+                            Please provide a valid state.
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <label for="zip" class="form-label">Zip</label>
+                        <input type="text" class="form-control" id="zip" placeholder="" required="" fdprocessedid="ypop05">
+                        <div class="invalid-feedback">
+                            Zip code required.
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+
+            <div class="section" id="watchlists">
+                user watchlists
+            </div>
+
+            <div class="section" id="your_lists">
+                user your_lists
+            </div>
+
+            <div class="section" id="account_settings">
+                user account_settings
+            </div>
+
+            <div class="section" id="logout">
+                user logout
+            </div>
         </div>
     </div>
     <?php include 'footer.php' ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="../js/userAccount.js"> </script>
 </body>
 
 </html>
